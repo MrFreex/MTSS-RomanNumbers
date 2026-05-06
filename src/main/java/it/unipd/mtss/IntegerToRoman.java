c35caaa5ebf2f124b0d8849cc03512f75f11e779
MrFreex <filippolissandrin@gmail.com>
2026-05-06 18:23:00 +0200

    feat: 1-3 number support

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -5,9 +5,34 @@
 
 
 package it.unipd.mtss;
+/**
+ * Class that converts an integer number to a roman number
+ * @author Filippo Lissandrin
+ */
 public class IntegerToRoman {
+  static final String[] UNITS = {"V", "I"};
+  static final int[] UNIT_VALUES = {5, 1};
+  static final int MAX_VALUE = 8;
+  /**
+   * Method that converts an integer number to a roman number
+   * @param number the integer number to convert
+   * @return the roman number as a string
+   * @throws IllegalArgumentException if the number is less than 1 or greater than 8 (bigger numbers not implemented yet)
+   */
   public static String convert(int number){
-  // TODO
-  return null;
+    if (number < 1 || number > MAX_VALUE) {
+      throw new IllegalArgumentException("Number must be between 1 and " + MAX_VALUE);
+    }
+
+    String s = "";
+    
+    for (int i = 0; i < UNITS.length; i++) {
+      while (number >= UNIT_VALUES[i]) {
+        s += UNITS[i];
+        number -= UNIT_VALUES[i];
+      }
+    }
+
+    return s;
   }
 }
