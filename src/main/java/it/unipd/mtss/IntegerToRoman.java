87cf4c992727f980863bc98de5556fd5d20ec591
monniale <94807502+monniale@users.noreply.github.com>
2026-05-06 17:41:57 +0200

    feat: Classes required schema

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -2,16 +2,12 @@
 // Alessandro Monni 2138005
 // Filippo Lissandrin 2137980
 ////////////////////////////////////////////////////////////////////
-package it.unipd.mtss;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+package it.unipd.mtss;
+public class IntegerToRoman {
+  public static String convert(int number){
+  // TODO
+  return null;
+  }
 }
